f83b0e7e95fc4484f2dcf77acce919479f57ebb8
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-05 13:33:50 +0200

    Initial Commit

---
+++ NotizenApp Watch App/NotizenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotizenAppApp.swift
+//  NotizenApp Watch App
+//
+//  Created by Azizbek Asadov on 05.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotizenApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotizenApp Watch AppTests/NotizenApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotizenApp_Watch_AppTests.swift
+//  NotizenApp Watch AppTests
+//
+//  Created by Azizbek Asadov on 05.10.2025.
+//
+
+import Testing
+@testable import NotizenApp_Watch_App
+
+struct NotizenApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
